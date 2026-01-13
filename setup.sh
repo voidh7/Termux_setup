55732a0bccaff22eeb16b1c581856514c7cd9e8f
DarkZero <ruanvsd2024@gmail.com>
2026-01-12 23:55:49 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,12 +12,5 @@ pkg install -y nmap
 pkg install -y whois
 pkg install -y iproute2
 pkg install -y python3
-pkg install -y php
-
-# 🐍 Instala bibliotecas Python úteis
-pip install requests
-pip install pyfiglet
-pip install colorama
-pip install flask
 
 echo "✅ Ambiente Termux configurado com sucesso!"
